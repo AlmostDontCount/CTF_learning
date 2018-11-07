1e0639b56dfb16b3a555adb9ffb009477920e32e
AlmostDontCount <1245550993@qq.com>
2018-11-07 14:12:27 +0800

    Update set1_1py3.py

--- crypto/cryptopals/set1/set1_1py3.py
+++ crypto/cryptopals/set1/set1_1py3.py
@@ -2,5 +2,6 @@
 #convert hex to base64
 import codecs
 code = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-b64 = codecs.encode(codecs.decode(code, 'hex'), 'base64').decode()
+#b64 = codecs.encode(codecs.decode(code, 'hex'), 'base64').decode()
+b64 = codecs.encode(codecs.decode(code, 'hex'), 'base64')
 print (b64)
